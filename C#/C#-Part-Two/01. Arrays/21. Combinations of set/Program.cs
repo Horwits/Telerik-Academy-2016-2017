6240f7a8c8e9cc69f2d9ed4d4840b8f1a53c5ccd
Horwits <horwits1@hotmail.com>
2016-04-08 18:52:08 +0300

    Added a solution for Combinations in Arrays homework.

---
+++ C#/C#-Part-Two/01. Arrays/21. Combinations of set/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static string ConvertArayToString<T>(T[] array)
+    {
+        return string.Format("{0}", "{" + string.Join(" ,", array) + "}");
+    }
+
+    static void Combinations(int[] array, int index, int currentNumber, int elements)
+    {
+        int length = array.Length;
+        if (index == length)
+        {
+            Console.WriteLine(ConvertArayToString(array));
+        }
+        else
+        {
+            for (int i = currentNumber; i <= elements; i++)
+            {
+                array[index] = i;
+                Combinations(array, index + 1, i + 1, elements);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[k];
+
+        Combinations(arr, 0, 1, n);
+    }
+}
