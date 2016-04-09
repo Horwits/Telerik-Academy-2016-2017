56feeb4aabecc299506f495176efb5277ba6ba69
Horwits <horwits1@hotmail.com>
2016-04-09 19:35:18 +0300

    Added new folder and solution for Methods, and a solution for the first problem.

---
+++ C#/C#-Part-Two/03. Methods/01. Say Hello/NamePrinter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class NamePrinter
+{
+    static void PrintNameOnConsole(string name)
+    {
+        Console.WriteLine("Hello, {0}!", name);
+    }
+
+    static void Main()
+    {
+        var userInput = Console.ReadLine();
+        PrintNameOnConsole(userInput);
+    }
+}
