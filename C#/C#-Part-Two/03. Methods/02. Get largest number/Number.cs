8d8343e5812e86dbddd52700d15e5eac4d2495c1
Horwits <horwits1@hotmail.com>
2016-04-09 19:53:01 +0300

    Added solution for Get largest number in Methods

---
+++ C#/C#-Part-Two/03. Methods/02. Get largest number/Number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Number
+{
+    static int GetMax(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else if (b > a)
+        {
+            return b;
+        }
+
+        return a;
+    }
+
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        int c = int.Parse(input[2]);
+
+        if (a == GetMax(a, b))
+        {
+            Console.WriteLine(GetMax(a, c));
+        }
+        else if (b == GetMax(a, b))
+        {
+            Console.WriteLine(GetMax(b, c));
+        }
+    }
+}
