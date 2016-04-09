c6232330e75739bf77785c37c8d4e36fa4cc256e
Horwits <horwits1@hotmail.com>
2016-04-09 19:47:09 +0300

    Added Multidimensional arrays solutions

---
+++ C#/C#-Part-Two/02. Multidimensional Arrays/02. Maximal sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string heightAndWidth = Console.ReadLine();
+        var splitted = heightAndWidth.Split(' ');
+        int n = int.Parse(splitted[0]);
+        int m = int.Parse(splitted[1]);
+
+        decimal sum = 0;
+        string[,] matrix = new string[n, m];
+        for (int i = 0, j = 0; i < n; i++)
+        {
+            if (j <= m)
+            {
+                var input = Console.ReadLine().Split(' ');
+                foreach (var item in input)
+                {
+                    matrix[i, j] = item;
+                    sum += int.Parse(item);
+                    j++;
+                }
+            }
+
+            j = 0;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
