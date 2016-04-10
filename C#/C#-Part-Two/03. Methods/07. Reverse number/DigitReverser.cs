af7fc96584b15daa5f071953c27d1bf5d3a24f8d
Horwits <horwits1@hotmail.com>
2016-04-10 18:16:09 +0300

    Added solution for Reverse Number in Methods topic.

---
+++ C#/C#-Part-Two/03. Methods/07. Reverse number/DigitReverser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+class DigitReverser
+{
+    static void PrintReversedNumberToConsole(decimal number)
+    {
+        Console.WriteLine(ReverseNumber(number));
+    }
+
+    static decimal ReverseNumber(decimal number)
+    {
+        var numberToString = number.ToString().ToCharArray();
+        var reversedToString = new StringBuilder();
+
+        int length = numberToString.Length;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            reversedToString.Append(numberToString[i]);
+        }
+
+        number = decimal.Parse(reversedToString.ToString());
+
+        return number;
+    }
+
+    static void Main()
+    {
+        decimal number = decimal.Parse(Console.ReadLine());
+
+        PrintReversedNumberToConsole(number);
+    }
+}
