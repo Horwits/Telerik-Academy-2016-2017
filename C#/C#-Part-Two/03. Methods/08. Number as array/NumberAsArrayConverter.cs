28802a6ad7d05103b75288b55202327f44b4ef5e
Horwits <horwits1@hotmail.com>
2016-04-11 19:00:22 +0300

    Added solution for 08. Number as array in Methods.

---
+++ C#/C#-Part-Two/03. Methods/08. Number as array/NumberAsArrayConverter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+class NumberAsArrayConverter
+{
+    static void PrintNumberSeparatedBySpacesToConsole(string number)
+    {
+        var numberToArray = number.ToCharArray();
+        Console.WriteLine(string.Join(" ", numberToArray));
+    }
+
+    static string CalculateTwoNumbersRepresentedAsArrays(string[] firstNumber, string[] secondNumber, short numberOneLength, short numberTwoLength)
+    {
+        var result = new StringBuilder();
+
+        var maxLength = Math.Max(numberOneLength, numberTwoLength);
+
+        short firstDigit = 0,
+            secondDigit = 0,
+            beforeSum = 0;
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            if (numberOneLength != maxLength)
+            {
+                secondDigit = short.Parse(secondNumber[i]);
+
+                if (i < numberOneLength)
+                {
+                    firstDigit = short.Parse(firstNumber[i]);
+                }
+                else
+                {
+                    firstDigit = 0;
+                }
+            }
+            else
+            {
+                firstDigit = short.Parse(firstNumber[i]);
+
+                if (i < numberTwoLength)
+                {
+                    secondDigit = short.Parse(secondNumber[i]);
+                }
+                else
+                {
+                    secondDigit = 0;
+                }
+            }
+
+            short nextSum = (short)(firstDigit + secondDigit);
+            if (beforeSum >= 10)
+            {
+                nextSum++;
+            }
+            if (nextSum >= 10)
+            {
+                result.Append(nextSum % 10);
+                beforeSum = nextSum;
+            }
+            else
+            {
+                result.Append(nextSum);
+                beforeSum = 0;
+            }
+        }
+
+        return result.ToString();
+    }
+
+    static void Main()
+    {
+        var lengths = Console.ReadLine().Split(' ');
+
+        short numberOneLength = short.Parse(lengths[0]);
+        short numberTwoLength = short.Parse(lengths[1]);
+
+        var firstNumber = Console.ReadLine().Split(' ');
+        var secondNumber = Console.ReadLine().Split(' ');
+
+        var result = CalculateTwoNumbersRepresentedAsArrays(firstNumber, secondNumber, numberOneLength, numberTwoLength);
+
+        Console.Clear();
+        PrintNumberSeparatedBySpacesToConsole(result);
+    }
+}
+
