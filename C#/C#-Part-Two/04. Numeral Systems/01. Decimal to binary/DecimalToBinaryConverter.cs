c0081d17f2e19c218cc4c7a34152472a54a6ea6b
Horwits <horwits1@hotmail.com>
2016-04-12 17:16:25 +0300

    Added solutions for 01. Decimal to binary and 02. Binary to decimal in 04. Numeral Systems topic.

---
+++ C#/C#-Part-Two/04. Numeral Systems/01. Decimal to binary/DecimalToBinaryConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+class DecimalToBinaryConverter
+{
+    static string ConvertDecimalToBinary(long number)
+    {
+        if (number < 1)
+        {
+            throw new ArgumentException("Input number.", "Number is less than 1.");
+        }
+
+        var reversedResult = new StringBuilder();
+
+        while (number != 0)
+        {
+            int currentDigit = (int)(number % 2);
+            reversedResult.Append(currentDigit);
+
+            number /= 2;
+        }
+
+        var result = new StringBuilder();
+
+        int length = reversedResult.Length;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            result.Append(reversedResult[i]);
+        }
+
+        return result.ToString();
+    }
+
+    static void Main()
+    {
+        try
+        {
+            var number = long.Parse(Console.ReadLine());
+            Console.WriteLine(ConvertDecimalToBinary(number));
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+    }
+}
---
+++ C#/C#-Part-Two/04. Numeral Systems/02. Binary to decimal/BinaryToDecimalConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class BinaryToDecimalConverter
+{
+    static long ConvertBinaryToDecimal(string binary)
+    {
+        int currentPosition = 0,
+            length = binary.Length;
+
+        long decimalRepresentation = 0;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            int currentDigit = binary[i] - '0';
+            decimalRepresentation += (currentDigit * (long)Math.Pow(2, currentPosition));
+            currentPosition++;
+        }
+
+        return decimalRepresentation;
+    }
+
+    static void Main()
+    {
+        string binary = Console.ReadLine();
+
+        var result = ConvertBinaryToDecimal(binary);
+        Console.WriteLine(result);
+    }
+}
