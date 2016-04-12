53fd9518d9b899d0d076a9f58e0e3e8d33a017f0
Horwits <horwits1@hotmail.com>
2016-04-12 18:13:44 +0300

    Added solutions for 03. Decimal to hexadecimal and 04. Hexadecimal to decimal in 04. Numeral Systems topic.

---
+++ C#/C#-Part-Two/04. Numeral Systems/03. Decimal to hexadecimal/DecimalToHexadecimalConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+class DecimalToHexadecimalConverter
+{
+    static string ConvertDecimalToHex(long number)
+    {
+        var reversedResult = new StringBuilder();
+        while (number != 0)
+        {
+            int currentDigit = (int)(number % 16);
+            if (currentDigit < 10)
+            {
+                reversedResult.Append(currentDigit);
+            }
+            else
+            {
+                switch (currentDigit)
+                {
+                    case 10: reversedResult.Append('A'); break;
+                    case 11: reversedResult.Append('B'); break;
+                    case 12: reversedResult.Append('C'); break;
+                    case 13: reversedResult.Append('D'); break;
+                    case 14: reversedResult.Append('E'); break;
+                    case 15: reversedResult.Append('F'); break;
+                }
+            }
+
+            number /= 16;
+        }
+
+        string result = reversedResult.ToString();
+        return result;
+    }
+
+    static string ReverseString(string input)
+    {
+        var reversedResult = new StringBuilder();
+        int length = input.Length;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            reversedResult.Append(input[i]);
+        }
+
+        string result = reversedResult.ToString();
+        return result;
+    }
+
+    static void Main()
+    {
+        //1000000000000000000
+        long number = long.Parse(Console.ReadLine());
+
+        var result = ConvertDecimalToHex(number);
+        Console.WriteLine(ReverseString(result));
+    }
+}
---
+++ C#/C#-Part-Two/04. Numeral Systems/04. Hexadecimal to decimal/HexadecimalToDecimalConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class HexadecimalToDecimalConverter
+{
+    static long ConvertHexToDecimal(string hex)
+    {
+        long result = 0;
+        int currentPosition = 0,
+            length = hex.Length;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            char currentChar = hex[i];
+            var currentNum = 0;
+            if (currentChar >= '0' && currentChar <= '9')
+            {
+                currentNum = currentChar - '0';
+            }
+            else
+            {
+                currentNum = currentChar - 'a' + 10;
+            }
+
+            result += (currentNum * (long)Math.Pow(16, currentPosition));
+            currentPosition++;
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        var hex = Console.ReadLine().ToLower();
+
+        var result = ConvertHexToDecimal(hex);
+        Console.WriteLine(result);
+    }
+}
