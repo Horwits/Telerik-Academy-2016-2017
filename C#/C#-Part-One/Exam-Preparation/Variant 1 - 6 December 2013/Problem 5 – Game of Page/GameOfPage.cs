686b222e8389a22e4a9d8721ea80b368973ab126
Horwits <horwits1@hotmail.com>
2016-04-14 18:54:58 +0300

    Added GameOfPage.cs in Variant 1 - 6 December 2013 + solution for Binary Short in 04 Numerical systems.

---
+++ C#/C#-Part-One/Exam-Preparation/Variant 1 - 6 December 2013/Problem 5 – Game of Page/GameOfPage.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Text;
+
+class GameOfPage
+{
+    static void Main()
+    {
+        StringBuilder[] pageTray = new StringBuilder[16];
+
+        for (int i = 0; i < 16; i++)
+        {
+            pageTray[i] = new StringBuilder();
+            pageTray[i].Append(Console.ReadLine());
+        }
+
+        decimal bill = 0;
+        while (true)
+        {
+            var command = Console.ReadLine();
+
+            if (command == "paypal")
+            {
+                Console.WriteLine("{0:f2}", bill);
+                break;
+            }
+
+            byte currentRow = byte.Parse(Console.ReadLine());
+            byte currentCol = byte.Parse(Console.ReadLine());
+
+            bool isWholeCookie =
+                currentRow + 1 < 16 &&
+                currentRow - 1 >= 0 &&
+                currentCol + 1 < 16 &&
+                currentCol - 1 >= 0 &&
+                (pageTray[currentRow][currentCol] == '1' &&
+                pageTray[currentRow][currentCol - 1] == '1' &&
+                pageTray[currentRow][currentCol + 1] == '1') &&
+                (pageTray[currentRow - 1][currentCol] == '1' &&
+                pageTray[currentRow - 1][currentCol - 1] == '1' &&
+                pageTray[currentRow - 1][currentCol + 1] == '1') &&
+                (pageTray[currentRow + 1][currentCol] == '1' &&
+                pageTray[currentRow + 1][currentCol - 1] == '1' &&
+                pageTray[currentRow + 1][currentCol + 1] == '1');
+
+            bool isCrumbTopLeft =
+                pageTray[currentRow][currentCol] == '1' &&
+                ((currentCol - 1 < 0 && currentRow - 1 < 0) &&
+                pageTray[currentRow][currentCol + 1] == '0' &&
+                pageTray[currentRow + 1][currentCol] == '0' &&
+                pageTray[currentRow + 1][currentCol + 1] == '0');
+
+            bool isCrumbTopRight =
+                pageTray[currentRow][currentCol] == '1' &&
+                ((currentCol + 1 > 15 && currentRow - 1 < 0) &&
+                pageTray[currentRow][currentCol - 1] == '0' &&
+                pageTray[currentRow + 1][currentCol] == '0' &&
+                pageTray[currentRow + 1][currentCol - 1] == '0');
+
+            bool isCrumbBottomLeft =
+                pageTray[currentRow][currentCol] == '1' &&
+                ((currentCol - 1 < 0 && currentRow + 1 > 15) &&
+                pageTray[currentRow][currentCol + 1] == '0' &&
+                pageTray[currentRow - 1][currentCol] == '0' &&
+                pageTray[currentRow - 1][currentCol + 1] == '0');
+
+            bool isCrumbBottomRight =
+                pageTray[currentRow][currentCol] == '1' &&
+                ((currentCol + 1 > 15 && currentRow + 1 > 15) &&
+                pageTray[currentRow][currentCol - 1] == '0' &&
+                pageTray[currentRow - 1][currentCol] == '0' &&
+                pageTray[currentRow - 1][currentCol - 1] == '0');
+
+            bool isCrumbWithoutTop =
+                pageTray[currentRow][currentCol] == '1' &&
+                ((currentCol + 1 < 16 && currentCol - 1 >= 0 && currentRow - 1 < 0)) &&
+                ((pageTray[currentRow][currentCol - 1] == '0' &&
+                pageTray[currentRow][currentCol + 1] == '0') &&
+                (pageTray[currentRow + 1][currentCol] == '0' &&
+                pageTray[currentRow + 1][currentCol - 1] == '0' &&
+                pageTray[currentRow + 1][currentCol + 1] == '0'));
+
+            bool isCrumbWithoutBottom =
+                pageTray[currentRow][currentCol] == '1' &&
+                ((currentCol + 1 < 16 && currentCol - 1 >= 0 && currentRow + 1 > 15)) &&
+                ((pageTray[currentRow][currentCol - 1] == '0' &&
+                pageTray[currentRow][currentCol + 1] == '0') &&
+                (pageTray[currentRow - 1][currentCol] == '0' &&
+                pageTray[currentRow - 1][currentCol - 1] == '0' &&
+                pageTray[currentRow - 1][currentCol + 1] == '0'));
+
+            bool isCrumb =
+                currentRow + 1 < 16 &&
+                currentRow - 1 >= 0 &&
+                currentCol + 1 < 16 &&
+                currentCol - 1 >= 0 &&
+                (pageTray[currentRow][currentCol] == '1' &&
+                pageTray[currentRow][currentCol - 1] == '0' &&
+                pageTray[currentRow][currentCol + 1] == '0') &&
+                (pageTray[currentRow - 1][currentCol] == '0' &&
+                pageTray[currentRow - 1][currentCol - 1] == '0' &&
+                pageTray[currentRow - 1][currentCol + 1] == '0') &&
+                (pageTray[currentRow + 1][currentCol] == '0' &&
+                pageTray[currentRow + 1][currentCol - 1] == '0' &&
+                pageTray[currentRow + 1][currentCol + 1] == '0');
+
+            char currentCentralPosition = pageTray[currentRow][currentCol];
+
+            if (command == "what is" && currentCentralPosition == '1')
+            {
+                if (isWholeCookie)
+                {
+                    Console.WriteLine("cookie");
+                }
+                else if (
+                    isCrumb ||
+                    isCrumbTopLeft ||
+                    isCrumbTopRight ||
+                    isCrumbBottomLeft ||
+                    isCrumbBottomRight ||
+                    isCrumbWithoutTop ||
+                    isCrumbWithoutBottom
+                    )
+                {
+                    Console.WriteLine("cookie crumb");
+                }
+                else
+                {
+                    Console.WriteLine("broken cookie");
+                }
+            }
+
+            if (command == "buy")
+            {
+                if (isWholeCookie)
+                {
+                    decimal cookieValue = 1.79m;
+                    bill += cookieValue;
+
+                    pageTray[currentRow][currentCol] = '0';
+                    pageTray[currentRow][currentCol - 1] = '0';
+                    pageTray[currentRow][currentCol + 1] = '0';
+                    pageTray[currentRow - 1][currentCol] = '0';
+                    pageTray[currentRow - 1][currentCol - 1] = '0';
+                    pageTray[currentRow - 1][currentCol + 1] = '0';
+                    pageTray[currentRow + 1][currentCol] = '0';
+                    pageTray[currentRow + 1][currentCol - 1] = '0';
+                    pageTray[currentRow + 1][currentCol + 1] = '0';
+                }
+                else
+                {
+                    Console.WriteLine("page");
+                }
+            }
+
+            if (currentCentralPosition == '0' && command != "buy")
+            {
+                Console.WriteLine("smile");
+            }
+        }
+    }
+}
---
+++ C#/C#-Part-Two/04. Numeral Systems/08. Binary short/BinaryShortGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+class BinaryShortGenerator
+{
+	//TODO: Signed solution fix
+    static string ReverseString(string input)
+    {
+        int length = input.Length;
+        var result = new StringBuilder();
+        for (int i = length - 1; i >= 0; i--)
+        {
+            result.Append(input[i]);
+        }
+
+        return result.ToString();
+    }
+
+    static void Main()
+    {
+        short n = short.Parse(Console.ReadLine());
+        var copyN = n;
+
+        var binaryResult = new StringBuilder();
+        while (copyN != 0)
+        {
+            int digit = copyN % 2;
+            copyN /= 2;
+
+            if (n < 0)
+            {
+                if (digit == -1)
+                {
+                    binaryResult.Append(0);
+                }
+                else
+                {
+                    binaryResult.Append(1);
+                }
+
+            }
+            else
+            {
+                binaryResult.Append(digit);
+            }
+        }
+
+        if (n < 0)
+        {
+            int length = binaryResult.Length;
+            binaryResult = binaryResult.Remove(0, 1);
+
+            string revertedResult = binaryResult.ToString();
+
+            Console.WriteLine(ReverseString(revertedResult) + '1');
+        }
+        else
+        {
+            string revertedResult = binaryResult.ToString();
+            Console.WriteLine(ReverseString(revertedResult).PadLeft(16, '0'));
+        }
+    }
+}
