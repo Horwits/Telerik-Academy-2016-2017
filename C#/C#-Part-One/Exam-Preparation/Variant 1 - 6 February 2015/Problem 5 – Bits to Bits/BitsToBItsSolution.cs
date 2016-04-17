ba50f0b59322861970256415634f0745253307ac
Horwits <horwits1@hotmail.com>
2016-04-17 23:11:12 +0300

    Added exam task solutions for c# 1.

---
+++ C#/C#-Part-One/Exam-Preparation/Variant 1 - 6 February 2015/Problem 5 – Bits to Bits/BitsToBItsSolution.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+class BitsToBItsSolution
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        var numbers = new string[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = Convert.ToString(int.Parse(Console.ReadLine()), 2).PadLeft(30, '0');
+        }
+
+        int maxLengthOfZeroes = 0,
+            maxLengthOfOnes = 0;
+
+        int currentLengthOfZeroes = 0,
+            currentLengthOfOnes = 0;
+
+        bool isCurrentSeqOfOnesContinues = false,
+            isCurrentSeqOFZeroesContinues = false;
+
+        for (int row = 0; row < n; row++)
+        {
+            int length = 30;
+
+            for (int currentPos = 0; currentPos < 30; currentPos++)
+            {
+                int currentPosPlusOne = currentPos + 1;
+
+                char currentDigit = numbers[row][currentPos];
+                char nextDigit = 'X';
+                if (currentPosPlusOne >= length)
+                {
+                    nextDigit = 'X';
+                }
+                else
+                {
+                    nextDigit = numbers[row][currentPosPlusOne];
+                }
+
+                bool isNextRowZeroPosValueOne = row + 1 < n && numbers[row + 1][0] == '1',
+                    isNextRowZeroPosValueZero = row + 1 < n && numbers[row + 1][0] == '0';
+
+                if ((currentDigit == '1'))
+                {
+                    currentLengthOfOnes++;
+                    currentLengthOfZeroes = 0;
+
+                    if (currentPos == length - 1 && isNextRowZeroPosValueOne)
+                    {
+                        isCurrentSeqOfOnesContinues = true;
+                    }
+                    else
+                    {
+                        isCurrentSeqOfOnesContinues = false;
+                    }
+
+                    if (currentDigit == nextDigit)
+                    {
+                        continue;
+                    }
+                }
+                else if ((currentDigit == '0'))
+                {
+                    currentLengthOfZeroes++;
+                    currentLengthOfOnes = 0;
+
+                    if (currentPos == length - 1 && isNextRowZeroPosValueZero)
+                    {
+                        isCurrentSeqOFZeroesContinues = true;
+                    }
+                    else
+                    {
+                        isCurrentSeqOFZeroesContinues = false;
+                    }
+
+                    if (currentDigit == nextDigit)
+                    {
+                        continue;
+                    }
+                }
+
+                if (currentLengthOfOnes > maxLengthOfOnes)
+                {
+                    maxLengthOfOnes = currentLengthOfOnes;
+                }
+
+                if (currentLengthOfZeroes > maxLengthOfZeroes)
+                {
+                    maxLengthOfZeroes = currentLengthOfZeroes;
+                }
+            }
+        }
+
+        Console.WriteLine(maxLengthOfZeroes);
+        Console.WriteLine(maxLengthOfOnes);
+    }
+}
---
+++ C#/C#-Part-One/Exam-Preparation/Variant 1 2 February 2015/Problem 5 – Sequences of Bits/SequencesOfBitsSolution.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+class SequencesOfBitsSolution
+{
+    //TODO: REMOVE UNNECESSERY CHECKS 
+
+    static int CountLongestSetBitsSequenceInArrayOfNumbers(uint[]numbers,int numbersLength)
+    {
+        int currentOneLength = 0,
+            maxOneLength = 0;
+
+        bool isSequenceOneContinue = false;
+        for (int row = numbersLength - 1; row >= 0; row--)
+        {
+            if (!isSequenceOneContinue)
+            {
+                currentOneLength = 0;
+            }
+            uint currentNum = numbers[row];
+            for (int pos = 0; pos < 30; pos++)
+            {
+                bool isBitOne = (currentNum & (1 << pos)) != 0;
+                if (isBitOne)
+                {
+                    currentOneLength++;
+                    int posPlusOne = pos + 1;
+
+                    if (row - 1 >= 0 && pos == 29)
+                    {
+                        bool bitOnNextRowIsOne = (numbers[row - 1] & (1 << 0)) != 0;
+                        if (bitOnNextRowIsOne)
+                        {
+                            isSequenceOneContinue = true;
+                        }
+                    }
+
+                    bool isBitOneOnPositionPlusOne = (currentNum & (1 << posPlusOne)) != 0;
+
+                    if (pos != 29 && !isBitOneOnPositionPlusOne)
+                    {
+                        isSequenceOneContinue = false;
+                    }
+
+                    if (isBitOneOnPositionPlusOne && pos != 29)
+                    {
+                        continue;
+                    }
+                    else if (!isBitOneOnPositionPlusOne && !isSequenceOneContinue)
+                    {
+                        if (currentOneLength > maxOneLength)
+                        {
+                            maxOneLength = currentOneLength;
+                        }
+
+                        currentOneLength = 0;
+                        isSequenceOneContinue = false;
+                    }
+                    else if (!isBitOneOnPositionPlusOne && row == 0 && pos == 29)
+                    {
+                        if (currentOneLength > maxOneLength)
+                        {
+                            maxOneLength = currentOneLength;
+                        }
+
+                        currentOneLength = 0;
+                        isSequenceOneContinue = false;
+                    }
+                }
+                else
+                {
+                    if (currentOneLength > maxOneLength)
+                    {
+                        maxOneLength = currentOneLength;
+                    }
+
+                    currentOneLength = 0;
+                    isSequenceOneContinue = false;
+                }
+            }
+        }
+
+        return maxOneLength;
+    }
+
+    static int CountLongestZeroBitSequenceInArrayOfNumbers(uint[] numbers, int numbersLength)
+    {
+        int currentLength = 0,
+            maxLength = 0;
+
+        bool isSequenceContinue = false;
+        for (int row = numbersLength - 1; row >= 0; row--)
+        {
+            if (!isSequenceContinue)
+            {
+                currentLength = 0;
+            }
+
+            uint currentNum = numbers[row];
+            for (int pos = 0; pos < 30; pos++)
+            {
+                bool isBitZero = (currentNum & (1 << pos)) == 0;
+                if (isBitZero)
+                {
+                    currentLength++;
+                    int posPlusOne = pos + 1;
+
+                    if (row - 1 >= 0 && pos == 29)
+                    {
+                        bool bitOnNextRowIsZero = (numbers[row - 1] & (1 << 0)) == 0;
+                        if (bitOnNextRowIsZero)
+                        {
+                            isSequenceContinue = true;
+                        }
+                        else
+                        {
+                            isSequenceContinue = false;
+                        }
+                    }
+
+                    bool isBitZeroOnPositionPlusOne = (currentNum & (1 << posPlusOne)) == 0;
+
+                    if (isBitZeroOnPositionPlusOne && pos != 29)
+                    {
+                        continue;
+                    }
+                    else if (pos == 29 && isBitZeroOnPositionPlusOne && row == 0)
+                    {
+                        if (currentLength > maxLength)
+                        {
+                            maxLength = currentLength;
+                        }
+                    }
+                    else if (pos == 29 && isBitZeroOnPositionPlusOne && !isSequenceContinue)
+                    {
+                        if (currentLength > maxLength)
+                        {
+                            maxLength = currentLength;
+                        }
+                    }
+                    else if (!isBitZeroOnPositionPlusOne && !isSequenceContinue)
+                    {
+                        if (currentLength > maxLength)
+                        {
+                            maxLength = currentLength;
+                        }
+
+                        currentLength = 0;
+                        isSequenceContinue = false;
+                    }
+                }
+                else
+                {
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                    }
+                    if (!isSequenceContinue)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        currentLength = 0;
+                        isSequenceContinue = false;
+                    }
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        uint[] numbers = new uint[n];
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = uint.Parse(Console.ReadLine());
+        }
+
+        int maxOneLength = CountLongestSetBitsSequenceInArrayOfNumbers(numbers, n);
+        int maxZeroLength = CountLongestZeroBitSequenceInArrayOfNumbers(numbers, n);
+        Console.WriteLine(maxOneLength);
+        Console.WriteLine(maxZeroLength);
+    }
+}
