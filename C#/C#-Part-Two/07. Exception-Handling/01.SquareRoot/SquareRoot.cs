730fec61977bcdc522ab90e30af7cf36f6a2dbcf
Daniel Nikolov <horwits1@hotmail.com>
2016-04-28 23:26:48 +0300

    Added folders, solution for the first problem in Exception handling

---
+++ C#/C#-Part-Two/07. Exception-Handling/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class SquareRoot
+{
+    public static double CalculateSquareRootOfInteger(string inputNumber)
+    {
+        int number = 0;
+        bool isNumberValid = int.TryParse(inputNumber, out number);
+
+        double squareRoot = 0;
+        if (isNumberValid)
+        {
+            squareRoot = Math.Sqrt(number);
+        }
+        else
+        {
+            if (!isNumberValid)
+            {
+                throw new ArgumentException("Invalid number");
+            }
+
+            if (number > 0)
+            {
+                throw new FormatException("Invalid number");
+            }
+        }
+
+        return squareRoot;
+    }
+
+    static void Main()
+    {
+        try
+        {
+            double sqrt = CalculateSquareRootOfInteger(Console.ReadLine());
+            Console.WriteLine(sqrt);
+        }
+        catch (ArgumentException ae)
+        {
+            Console.WriteLine(ae.Message);
+        }
+        catch (FormatException fe)
+        {
+            Console.WriteLine(fe.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
