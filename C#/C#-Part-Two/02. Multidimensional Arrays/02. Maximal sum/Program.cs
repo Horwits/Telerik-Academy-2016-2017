67ae15c1098db34cf8a400ce6f665f85cf32c0ad
Daniel Nikolov <horwits1@hotmail.com>
2016-05-07 16:28:47 +0300

    Added solutions for 02. Multidimentional Arrays

--- C#/C#-Part-Two/02. Multidimensional Arrays/02. Maximal sum/Program.cs
+++ C#/C#-Part-Two/02. Multidimensional Arrays/02. Maximal sum/Program.cs
@@ -2,6 +2,43 @@
 
 class Program
 {
+    static int[,] GetMatrixFromInput(int n, int m)
+    {
+        int[,] matrix = new int[n, m];
+
+        for (int i = 0; i < n; i++)
+        {
+            var currentInputRow = Console.ReadLine().Split(' ');
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = int.Parse(currentInputRow[j]);
+            }
+        }
+
+        return matrix;
+    }
+
+    //private static void PrintMatrix(decimal[,] matrix, int n, int m)
+    //{
+    //    for (int i = 0; i < n; i++)
+    //    {
+    //        for (int j = 0; j < m; j++)
+    //        {
+    //            if (j == n - 1)
+    //            {
+    //                Console.Write("{0}", matrix[i, j]);
+    //            }
+    //            else
+    //            {
+    //                Console.Write("{0} ", matrix[i, j]);
+    //            }
+    //        }
+    //
+    //        Console.WriteLine();
+    //    }
+    //
+    //}
+
     static void Main()
     {
         string heightAndWidth = Console.ReadLine();
@@ -9,22 +46,30 @@ class Program
         int n = int.Parse(splitted[0]);
         int m = int.Parse(splitted[1]);
 
-        decimal sum = 0;
-        string[,] matrix = new string[n, m];
-        for (int i = 0, j = 0; i < n; i++)
-        {
-            if (j <= m)
-            {
-                var input = Console.ReadLine().Split(' ');
-                foreach (var item in input)
-                {
-                    matrix[i, j] = item;
-                    sum += int.Parse(item);
-                    j++;
-                }
-            }
+        var matrix = GetMatrixFromInput(n, m);
 
-            j = 0;
+        int sum = int.MinValue;
+        for (int row = 0; row < n; row++)
+        {
+            int currentSum = 0;
+            for (int col = 0; col < m; col++)
+            {
+                if (col + 2 < m && row + 2 < n)
+                {
+                    currentSum =
+                        matrix[row, col] +
+                        matrix[row, col + 1] +
+                        matrix[row, col + 2] +
+                         matrix[row + 1, col] +
+                        matrix[row + 1, col + 1] +
+                        matrix[row + 1, col + 2] +
+                        matrix[row + 2, col] +
+                        matrix[row + 2, col + 1] +
+                        matrix[row + 2, col + 2];
+                }
+
+                sum = Math.Max(sum, currentSum);
+            }
         }
 
         Console.WriteLine(sum);
---
+++ C#/C#-Part-Two/02. Multidimensional Arrays/03. Sequence in matrix/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+class Program
+{
+    static string[,] GetMatrixFromInput(int n, int m)
+    {
+        var matrix = new string[n, m];
+
+        for (int i = 0; i < n; i++)
+        {
+            var currentInputRow = Console.ReadLine().Split(' ');
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = currentInputRow[j];
+            }
+        }
+
+        return matrix;
+    }
+
+    static int GetLongestColumnSequenceLength(string[,] matrix)
+    {
+        int length = 1,
+            maxLength = 1,
+            col = 0,
+            row = 0;
+
+        while (true)
+        {
+            if (row == matrix.GetLength(0) - 1)
+            {
+                col++;
+                if (col == matrix.GetLength(1) - 1)
+                {
+                    break;
+                }
+
+                row = 0;
+
+                length = 1;
+                continue;
+            }
+
+            var currentElement = matrix[row, col];
+            var nextElement = matrix[row + 1, col];
+
+            if (currentElement == nextElement)
+            {
+                length++;
+            }
+            else
+            {
+                length = 1;
+            }
+
+            maxLength = Math.Max(length, maxLength);
+
+            row++;
+        }
+
+        return maxLength;
+    }
+
+    static int GetLongestRowSequenceLength(string[,] matrix)
+    {
+        int length = 1,
+            maxLength = 1,
+            col = 0,
+            row = 0;
+
+        while (true)
+        {
+            if (col == matrix.GetLength(1) - 1)
+            {
+                row++;
+                if (row == matrix.GetLength(0) - 1)
+                {
+                    break;
+                }
+
+                col = 0;
+
+                length = 1;
+                continue;
+            }
+
+            var currentElement = matrix[row, col];
+            var nextElement = matrix[row, col + 1];
+
+            if (currentElement == nextElement)
+            {
+                length++;
+            }
+            else
+            {
+                length = 1;
+            }
+
+            maxLength = Math.Max(length, maxLength);
+
+            col++;
+        }
+
+        return maxLength;
+    }
+
+    static int GetLongestLeftDiagonalSequenceLength(string[,] matrix)
+    {
+        int length = 1,
+            maxLength = 1,
+            col = 0,
+            row = 0;
+
+        while (row + 1 < matrix.GetLength(0))
+        {
+            var currentElement = matrix[row, col];
+            var nextElement = matrix[row + 1, col + 1];
+
+            if (currentElement == nextElement)
+            {
+                length++;
+            }
+            else
+            {
+                length = 1;
+            }
+
+            maxLength = Math.Max(length, maxLength);
+
+            row++;
+            col++;
+        }
+
+        return maxLength;
+    }
+
+    static int GetLongestRightDiagonalSequenceLength(string[,] matrix)
+    {
+        int length = 1,
+            maxLength = 1,
+            col = matrix.GetLength(1) - 1,
+            row = 0;
+
+        while (row + 1 < matrix.GetLength(0))
+        {
+            var currentElement = matrix[row, col];
+            var nextElement = matrix[row + 1, col - 1];
+
+            if (currentElement == nextElement)
+            {
+                length++;
+            }
+            else
+            {
+                length = 1;
+            }
+
+            maxLength = Math.Max(length, maxLength);
+
+            row++;
+            col--;
+        }
+
+        return maxLength;
+    }
+
+    static void Main()
+    {
+        string heightAndWidth = Console.ReadLine();
+        var splitted = heightAndWidth.Split(' ');
+        int n = int.Parse(splitted[0]);
+        int m = int.Parse(splitted[1]);
+
+        var matrix = GetMatrixFromInput(n, m);
+
+        var longestCol = GetLongestColumnSequenceLength(matrix);
+        var longestRow = GetLongestRowSequenceLength(matrix);
+        var longestLeftDiagonal = GetLongestLeftDiagonalSequenceLength(matrix);
+        var longestRightDiagonal = GetLongestRightDiagonalSequenceLength(matrix);
+
+        //Console.WriteLine(longestCol);
+        //Console.WriteLine(longestRow);
+        //Console.WriteLine(longestLeftDiagonal);
+        //Console.WriteLine(longestRightDiagonal);
+
+        var result = Math.Max(Math.Max(longestCol, longestRow), Math.Max(longestLeftDiagonal,longestRightDiagonal));
+        Console.WriteLine(result);
+    }
+}
