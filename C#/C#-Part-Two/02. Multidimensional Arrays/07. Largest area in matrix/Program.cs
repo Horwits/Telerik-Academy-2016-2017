dc34cd1ae03a34ebdf78e7e556d49f0c1de879d2
Daniel Nikolov <horwits1@hotmail.com>
2016-05-10 22:28:19 +0300

    Changed some of the solutions for Numeral Systems to work in BGCoder.

--- C#/C#-Part-Two/02. Multidimensional Arrays/07. Largest area in matrix/Program.cs
+++ C#/C#-Part-Two/02. Multidimensional Arrays/07. Largest area in matrix/Program.cs
@@ -1,12 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-class Program
+class LargestArea
 {
+    static bool[,] visited = new bool[1000, 1000];
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+    static int newX;
+    static int newY;
+
+    static int dfs(int[,] matrix, int x, int y, int value)
+    {
+        if (visited[x, y])
+        {
+            return 0;
+        }
+        else
+        {
+            visited[x, y] = true;
+            int best = 0;
+            int bestX = x, bestY = y;
+            for (int i = 0; i < 4; i++)
+            {
+
+                int modx = dx[i] + x;
+                int mody = dy[i] + y;
+
+                //if we are outside of our matrix
+                if (modx == -1 || modx >= matrix.GetLength(0) ||
+                    mody == -1 || mody >= matrix.GetLength(1))
+                {
+                    continue;
+                }
+
+                //if neighbour value is the same, "DFS" it
+                if (matrix[modx, mody] == value)
+                {
+                    int v = dfs(matrix, modx, mody, value);
+                    best += v;
+                }
+                newX = bestX;
+                newY = bestY;
+            }
+
+            return best + 1;
+        }
+    }
+
+    static int[,] GetMatrixFromInput(int n, int m)
+    {
+        int[,] matrix = new int[n, m];
+
+        for (int i = 0; i < n; i++)
+        {
+            var currentInputRow = Console.ReadLine().Split(' ');
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = int.Parse(currentInputRow[j]);
+            }
+        }
+
+        return matrix;
+    }
+
     static void Main()
     {
+        string heightAndWidth = Console.ReadLine();
+        var splitted = heightAndWidth.Split(' ');
+        int n = int.Parse(splitted[0]);
+        int m = int.Parse(splitted[1]);
+
+        var matrix = GetMatrixFromInput(n, m);
+
+        //int res = dfs(matrix, 0, 1, 3);
+        //Console.WriteLine(res);
+
+        //traverse the matrix in search for largest area
+        int current = 0;
+        int max = 0;
+        for (int rows = 0; rows < matrix.GetLength(0); rows++)
+        {
+            for (int cols = 0; cols < matrix.GetLength(1); cols++)
+            {
+                if (visited[rows, cols] == false)
+                {
+                    current = dfs(matrix, rows, cols, matrix[rows, cols]);
+                    if (current > max)
+                    {
+                        max = current;
+                    }
+                }
+            }
+        }
+        Console.WriteLine(max);
     }
 }
--- C#/C#-Part-Two/04. Numeral Systems/06. Binary to hexadecimal/BinaryToHexConverter.cs
+++ C#/C#-Part-Two/04. Numeral Systems/06. Binary to hexadecimal/BinaryToHexConverter.cs
@@ -3,79 +3,107 @@ using System.Text;
 
 class BinaryToHexConverter
 {
-    static string ConvertBinaryToHex(string binaryInput)
+    //static string ConvertBinaryToHex(string binaryInput)
+    //{
+    //    if (binaryInput.Length < 8)
+    //    {
+    //        binaryInput = binaryInput.PadLeft(8, '0');
+    //    }
+
+    //    var hexResult = new StringBuilder();
+
+    //    int length = binaryInput.Length;
+
+    //    for (int i = 0; i < length; i += 4)
+    //    {
+    //        string currentBin = binaryInput.Substring(i, 4);
+    //        switch (currentBin)
+    //        {
+    //            case "0000":
+    //                hexResult.Append("0");
+    //                break;
+    //            case "0001":
+    //                hexResult.Append("1");
+    //                break;
+    //            case "0010":
+    //                hexResult.Append("2");
+    //                break;
+    //            case "0011":
+    //                hexResult.Append("3");
+    //                break;
+    //            case "0100":
+    //                hexResult.Append("4");
+    //                break;
+    //            case "0101":
+    //                hexResult.Append("5");
+    //                break;
+    //            case "0110":
+    //                hexResult.Append("6");
+    //                break;
+    //            case "0111":
+    //                hexResult.Append("7");
+    //                break;
+    //            case "1000":
+    //                hexResult.Append("8");
+    //                break;
+    //            case "1001":
+    //                hexResult.Append("9");
+    //                break;
+    //            case "1010":
+    //                hexResult.Append("A");
+    //                break;
+    //            case "1011":
+    //                hexResult.Append("B");
+    //                break;
+    //            case "1100":
+    //                hexResult.Append("C");
+    //                break;
+    //            case "1101":
+    //                hexResult.Append("D");
+    //                break;
+    //            case "1110":
+    //                hexResult.Append("E");
+    //                break;
+    //            case "1111":
+    //                hexResult.Append("F");
+    //                break;
+    //        }
+    //    }
+
+    //    return hexResult.ToString();
+    //}
+
+    //public static string BinaryStringToHexString(string binary)
+    //{
+    //    StringBuilder result = new StringBuilder(binary.Length / 8 + 1);
+
+    //    // TODO: check all 1's or 0's... Will throw otherwise
+
+    //    int mod4Len = binary.Length % 8;
+    //    if (mod4Len != 0)
+    //    {
+    //        // pad to length multiple of 8
+    //        binary = binary.PadLeft(((binary.Length / 8) + 1) * 8, '0');
+    //    }
+
+    //    for (int i = 0; i < binary.Length; i += 8)
+    //    {
+    //        string eightBits = binary.Substring(i, 8);
+    //        result.AppendFormat("{0:X}", Convert.ToByte(eightBits, 2));
+    //    }
+
+    //    return result.ToString();
+    //}
+
+    static string HexConverted(string strBinary)
     {
-        if (binaryInput.Length < 8)
-        {
-            binaryInput = binaryInput.PadLeft(8, '0');
-        }
-
-        var hexResult = new StringBuilder();
-
-        int length = binaryInput.Length;
-
-        for (int i = 0; i < length; i += 4)
-        {
-            string currentBin = binaryInput.Substring(i, 4);
-            switch (currentBin)
-            {
-                case "0000":
-                    hexResult.Append("0");
-                    break;
-                case "0001":
-                    hexResult.Append("1");
-                    break;
-                case "0010":
-                    hexResult.Append("2");
-                    break;
-                case "0011":
-                    hexResult.Append("3");
-                    break;
-                case "0100":
-                    hexResult.Append("4");
-                    break;
-                case "0101":
-                    hexResult.Append("5");
-                    break;
-                case "0110":
-                    hexResult.Append("6");
-                    break;
-                case "0111":
-                    hexResult.Append("7");
-                    break;
-                case "1000":
-                    hexResult.Append("8");
-                    break;
-                case "1001":
-                    hexResult.Append("9");
-                    break;
-                case "1010":
-                    hexResult.Append("A");
-                    break;
-                case "1011":
-                    hexResult.Append("B");
-                    break;
-                case "1100":
-                    hexResult.Append("C");
-                    break;
-                case "1101":
-                    hexResult.Append("D");
-                    break;
-                case "1110":
-                    hexResult.Append("E");
-                    break;
-                case "1111":
-                    hexResult.Append("F");
-                    break;
-            }
-        }
-
-        return hexResult.ToString();
+        string strHex = Convert.ToInt64(strBinary, 2).ToString("X");
+        return strHex;
     }
 
     static void Main()
     {
         string bin = Console.ReadLine();
-        Console.WriteLine(ConvertBinaryToHex(bin));
+        Console.WriteLine(HexConverted(bin));
     }
 }
