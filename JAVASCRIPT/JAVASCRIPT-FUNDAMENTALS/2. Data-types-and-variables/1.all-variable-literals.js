1265dd7c916a3214e5c0cc0f1ebf6928ce992a32
Daniel Nikolov <horwits1@hotmail.com>
2016-06-06 22:21:02 +0300

    Added solutions for the first homework -  2.Data types and variables.

---
+++ JAVASCRIPT/JAVASCRIPT-FUNDAMENTALS/2. Data-types-and-variables/1.all-variable-literals.js
@@ -0,0 +1,17 @@
+var variable;
+console.log(typeof (variable));
+
+var text = 'text';
+console.log(typeof (text));
+
+var number = 1;
+console.log(typeof (number));
+
+var obj = {};
+console.log(typeof (obj));
+
+var isBool = true;
+console.log(typeof (isBool));
+
+var array = [];
+console.log(typeof (array));
---
+++ JAVASCRIPT/JAVASCRIPT-FUNDAMENTALS/2. Data-types-and-variables/4.type-of-null.js
@@ -0,0 +1,5 @@
+var nullVar = null,
+undVar;
+
+console.log('typeof(null) = ' + typeof(nullVar));
+console.log('typeof(undefined) = ' + typeof(undVar));
